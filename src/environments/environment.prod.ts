27e4187f3da17922e49e2d476ba510cb80f50c65
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2022-02-08 10:50:05 +0200

    Include firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyDv4S-mTtTc7l_470gml9h6_YRfhi8VhOI'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyDv4S-mTtTc7l_470gml9h6_YRfhi8VhOI'
 };
 
 /*
