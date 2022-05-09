5dfa6cce51da1c7491834a18492122bc4b1bca84
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2022-05-09 17:36:07 +0300

    safe router

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { User } from 'src/app/shared/interface';
 import { AuthService } from '../shared/services/auth.service';
 
@@ -12,14 +12,20 @@ import { AuthService } from '../shared/services/auth.service';
 export class LoginPageComponent implements OnInit {
   form: FormGroup
   submitted = false;
+  message: string
 
   constructor(
     public auth: AuthService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute 
   ) { }
 
   ngOnInit(): void {
-
+    this.route.queryParams.subscribe((params: Params) => {
+      if(params['loginAgain']){
+        this.message = 'Please enter credantial'
+      }
+    });
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.email, 
