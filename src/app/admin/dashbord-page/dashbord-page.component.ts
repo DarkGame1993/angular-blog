2f94d0dfa8572f93e3c016b5a5b1af7bc22a83e0
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2022-05-15 17:44:51 +0300

    Create dashbord page

--- src/app/admin/dashbord-page/dashbord-page.component.ts
+++ src/app/admin/dashbord-page/dashbord-page.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from 'src/app/shared/interface';
+import { PostsService } from 'src/app/shared/posts.service';
 
 @Component({
   selector: 'app-dashbord-page',
   templateUrl: './dashbord-page.component.html',
   styleUrls: ['./dashbord-page.component.scss']
 })
-export class DashbordPageComponent implements OnInit {
+export class DashbordPageComponent implements OnInit, OnDestroy {
+  posts: Post[]= [];
+  pSub: Subscription
 
-  constructor() { }
+  constructor(private postsService: PostsService) {
 
-  ngOnInit(): void {
   }
+  
+  ngOnInit(): void {
+    this.pSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts;
+    })
+  }
+
+  remove(id: string): void {
+    
+  }
+
+  ngOnDestroy(): void {
+    if(this.pSub) {
+      this.pSub.unsubscribe();
+    }
+  }
+
 }
