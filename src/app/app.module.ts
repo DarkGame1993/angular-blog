602d96d19e4eefdb24921f9edba8e5aa3e44e821
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2022-05-17 15:18:35 +0300

    register locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './shared/auth.interceptor';
+import { registerLocaleData } from '@angular/common';
+import uaLocale from '@angular/common/locales/ru-UA';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,9 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
 import { PostComponent } from './shared/components/post/post.component';
 import { SharedModule } from './shared/shared.module';
+import { AuthInterceptor } from './shared/auth.interceptor';
+
+registerLocaleData(uaLocale, 'ua')
 
 const INTERSEPTOR_PROVIDER = {
   provide: HTTP_INTERCEPTORS,
