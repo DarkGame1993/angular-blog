e7f7e454bb0969076d5b9f5ea85e2ee53cfde15d
Varerii Skorokhod <valeriiskorokhod@gmail.com>
2022-05-17 19:14:50 +0300

    Build proud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { PostPageComponent } from './post-page/post-page.component';
 import { PostComponent } from './shared/components/post/post.component';
 import { SharedModule } from './shared/shared.module';
 import { AuthInterceptor } from './shared/auth.interceptor';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 registerLocaleData(uaLocale, 'ua')
 
@@ -32,7 +34,13 @@ const INTERSEPTOR_PROVIDER = {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [INTERSEPTOR_PROVIDER],
   bootstrap: [AppComponent]
